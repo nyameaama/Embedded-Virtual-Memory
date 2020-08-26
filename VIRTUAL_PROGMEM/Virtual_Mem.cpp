d9e7ba35912db9274d29db666d1204f1f8c711b9
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2020-08-26 22:17:46 +0000

    init

---
+++ VIRTUAL_PROGMEM/Virtual_Mem.cpp
@@ -0,0 +1,40 @@
+/*MIT License
+
+Copyright (c) 2020 Nyameaama Gambrah
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+#include"Virtual_Mem.h"
+
+void VIRTUAL_MEMORY::createVariableContainer(char* variableName){
+    //PACKAGE_JSON *jsonObject = new PACKAGE_JSON();
+    //SD_API *sdObject = new SD_API();
+    //jsonObject -> addToJsonObject(StaticJsonDocument<200>file,variableName,0);
+
+}
+
+void VIRTUAL_MEMORY::deleteVariableContainer(char* variableName){
+
+}
+
+template<typename RT>
+RT VIRTUAL_MEMORY::addToVariableContainer(char* container, RT data){
+
+}
