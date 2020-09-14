47ebd15ae7cb4226dc94ae1765cc73566ec47ea2
Nyameaama Gambrah <nyameaama.gambra@yahoo.com>
2020-09-14 19:21:08 +0000

    added example file for PC-Files System

---
+++ examples/VirtualMemoryPC-FS.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<fstream>
+#include"../VIRTUAL_PROGMEM/Virtual_Mem.h"
+
+//Operation Functions prototypes:
+//Input
+char* inputDatafromFS(char* filename);
+//Output
+void outputDatatoFS(char* filename,char* data);
+//Create
+void createfileOnFS(char* filename);
+//Remove
+void deletefileOnFS(char* filename);
+
+
+int main(){
+    //Create Virtual Memory Object
+    //Add input function, output function, create function and delete function as parameters.
+    VIRTUAL_MEMORY *OperationObject = new VIRTUAL_MEMORY(inputDatafromFS,outputDatatoFS,createfileOnFS,deletefileOnFS);
+
+    //Create a file with filename "ExampleFilename" and placeholer data "ExampleData"
+    //Placeholder data can be of type char* and int
+    //OperationObject ->createVariableContainer("ExampleFilename","ExampleData");
+
+    //Modify data in a specified file
+    OperationObject ->modifyVariableContainer("EmbeddedVM_Test.txt","ExampleNewData");
+
+    //Retrieve data from a specified file 
+    OperationObject ->retrieveValueFromContainer("EmbeddedVM_Test.txt");
+
+
+}
+
+//Operation Functions
+//Input
+char* inputDatafromFS(char* filename){
+    std::ifstream tempFile;
+    char* data;
+    tempFile.open(filename,std::ios::in);
+    if(tempFile.is_open()){
+        while(!tempFile.eof()){
+            tempFile >> data;
+        }
+    }
+    tempFile.close();
+    return data;
+}
+//Output
+void outputDatatoFS(char* filename,char* data){
+    std::ofstream tempFile;
+    tempFile.open(filename,std::ios::out);
+    if(tempFile.is_open()){
+        tempFile << data;
+    }
+    tempFile.close();
+}
+//Create
+void createfileOnFS(char* filename){
+    std::fstream tempFile;
+    //Creating file in Prog Directory 
+    tempFile.open(filename,std::ios::out);
+    if(tempFile.is_open()){
+        //File created
+    }
+    tempFile.close();
+}
+//Remove
+void deletefileOnFS(char* filename){
+    remove(filename);
+}
